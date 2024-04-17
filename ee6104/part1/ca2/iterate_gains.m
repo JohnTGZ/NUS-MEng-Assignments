82f20b9482c6e43ade85e8895740d7ec1f3a0d32
johntgz <john_tanguanzhong@hotmail.com>
2024-04-18 00:42:21 +0800

    added temperature model for ee6104 ca2

--- ee6104/part1/ca2/iterate_gains.m
+++ ee6104/part1/ca2/iterate_gains.m
@@ -14,16 +14,20 @@ a_1 = 0.9897;
 b_1 = 0.05263;
 d_1 = 15;
 
-kc_1 = 1; % Proportional gain
-kI_1 = 1; % Integral gain
+% Temp model 1
+T_a_1 = 0.9832;
+T_b_1 = 0.03446;
+
+kc_1 = 0.5; % Proportional gain
+kI_1 = 0.01; % Integral gain
 
 % Hyperparameters
 step_i = 0.1 % Step size
 N = 20 % Sample size
 
-pd_y_rho = 
-
-R_i = (1/N) * 
-pd_J_rho = (1/N) * 
-
-rho_next = rho_i - step_i * inv(R_i) * pd_J_rho
+% pd_y_rho = 
+% 
+% R_i = (1/N) * 
+% pd_J_rho = (1/N) * 
+% 
+% rho_next = rho_i - step_i * inv(R_i) * pd_J_rho
